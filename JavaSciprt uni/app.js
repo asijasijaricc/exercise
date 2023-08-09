ede28f91b17894c085fe5513a43b8bfeebbbfb56
asijasijaricc <asijasijaricc12@gmail.com>
2023-08-09 18:03:35 +0200

    domaci

--- JavaSciprt uni/app.js
+++ JavaSciprt uni/app.js
@@ -1,1 +1,23 @@
 // RegEx
+
+function rigips(rigi) {
+  let reg = /([89][0-9]{9})/;
+  if (rigi === reg) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(rigips(893478912221));
+
+function rigips2(rigi) {
+  let reg = /([a-z][0-9][@][a-z]+[\.]+[a-z]{2,3})/;
+  if (rigi === reg) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+console.log(rigips2("asijasijaric123@gmail.com"));
