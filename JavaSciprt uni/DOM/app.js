5f4b9aab2a7dc9881684ce6508045efa7e59ad84
asijasijaricc <asijasijaricc12@gmail.com>
2023-08-13 16:45:30 +0200

    vezba

---
+++ JavaSciprt uni/DOM/app.js
@@ -0,0 +1,8 @@
+// Kreirati odeljak za JavaScript unutar zaglavlja stranice.
+// Kreirati JavaScript funkciju pocetak(), koja se izvršava kada se stranica učita.
+// Funkcija pocetak() menja tekst naziva iz zaglavlja dokumenta u "JS primer".
+// Kreirati funkciju promena() koja se izvršava u trenutku kada se klikne na H1 naslov dokumenta.
+// Funkcija promena() menja izgled teksta "Korekcija izgleda", tako što postavlja boju pozadine na #ffc, boju slova na crvenu, slova postaju podebljana i ispisana fontom sans-serif.
+// Kreirati funkcije ulaz(obj) i izlaz(obj) koje se izvršavaju u trenutku kada se miš postavi iznad pasusa "Promena klase i sadržaja", odnosno kada se miš ukloni i to tako da se prilikom poziva funkcije prosleđuje sam elemenat.
+// Funkcija ulaz(obj) zadatom objektu postavlja klasu "aktivan" i menja mu sadržaj u tekst "Miš preko elementa".
+// Funkcija izlaz(obj) zadatom objektu postavlja klasu "obican" i menja mu sadržaj u tekst "Promena klase i sadržaja".
